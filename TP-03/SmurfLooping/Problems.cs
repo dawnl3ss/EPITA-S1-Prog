60b274c0599d0d2fb0ff3735c3b1670d5d66c0f6
4m4Sec <neptune.dev.cybsec@gmail.com>
2023-10-16 23:22:59 +0200

    update [3.5.0] : finish TP-03

--- TP-03/SmurfLooping/Problems.cs
+++ TP-03/SmurfLooping/Problems.cs
@@ -50,33 +50,53 @@ public class Problems
         return tree;
     }
     
-    public static string KingOfTheHill(string s)
+    public static int KingOfTheHill(string s)
     {
-        int len = s.Length;
-        string sanitizedStr = "";
-        bool hasIncreased = false;
-        bool hasDecreased = false;
+        int king = -1;
+        int previous = -1;
+        bool increasing = false;
+        bool decreasing = false;
 
-        // Sanitized str by removing ' '
-        foreach (char chr in s)
+        foreach (char digit in s)
         {
-            if (chr != ' ')
-                sanitizedStr += chr;
+            if (digit == ' ')
+                continue;
+            
+            int current = digit - '0';
+
+            if (previous == -1)
+            {
+                previous = current;
+                king = current;
+            }
+            else if (current > previous)
+            {
+                if (decreasing)
+                    return -1;
+                
+                increasing = true;
+                king = current;
+                previous = current;
+            }
+            else if (current < previous)
+            {
+                decreasing = true;
+                previous = current;
+            }
+            else if (current == previous)
+            {
+                king = current;
+            }
         }
 
-        for (int i = 1; i <= len; i++)
-        {
-            if (sanitizedStr[i] > sanitizedStr[i - 1])
-                return "non";
-        }
-
-        return sanitizedStr;
+        return king;
     }
 
     public static string ItoA(int n)
     {
         if (n == 0)
             return "0";
+        
         string result = "";
         int nn = Int32.Abs(n);
         
@@ -85,23 +105,97 @@ public class Problems
             result = (Int32.Abs(nn) % 10) + result;
             nn /= 10;
         }
+        
         if (n < 0)
             result = "-" + result;
+        
         return result;
     }
 
     public static int AtoI(string s)
     {
+        int result = 0;
+        
         foreach (char chr in s)
         {
-            if (chr == ' ' && chr == ',' && chr == '!' && chr == '?' && chr == ':' && chr == '.')
+            if (chr == ' ' || chr == ',' || chr == '!' || chr == '?' || chr == ':' || chr == '.' || chr == '*')
                 throw new ArgumentException("Parameter 's' could not contain special characters.");
 
+            if (chr == '+' || chr == '-')
+                continue;
+            
+            result = result * 10 + (chr - '0');
         }
+
+        if (s[0] == '-')
+            result = -result;
+        
+        return result;
     }
+
+
+    public static string DecimalToBase16(ushort n)
+    {
+        if (n == 0)
+            return "0";
+
+        string hexchars = "0123456789ABCDEF";
+        string result = "";
+
+        while (n > 0)
+        {
+            int remainder = n % 16;
+            result = hexchars[remainder] + result;
+            n /= 16;
+        }
+
+        return result;
+    }
+
+
+   public static ushort Base16ToDecimal(string hexNumber)
+   {
+       if (hexNumber == null)
+           throw new ArgumentNullException("wrong hexnumber");
+   
+       ushort result = 0;
+       int power = 1;
+   
+       for (int i = hexNumber.Length - 1; i >= 0; i--)
+       {
+           char c = hexNumber[i];
+           int digitValue;
+   
+           if (c >= '0' && c <= '9')
+               digitValue = c - '0';
+           else if (c >= 'A' && c <= 'F')
+               digitValue = c - 'A' + 10;
+           else if (c >= 'a' && c <= 'f')
+               digitValue = c - 'a' + 10;
+           else
+               throw new FormatException("Invalid character in hexNumber");
+           
+           result += (ushort)(digitValue * power);
+           power *= 16;
+       }
+   
+       return result;
+   }
+
+    
+   
+   public static string HanoiTower(int n)
+   {
+       if (n < 1)
+           throw new ArgumentException("N should be upper than 0.");
+
+       return "";
+   }
+   
+   
 
     public static void Main()
     {
-        Console.WriteLine(Tree('*', -1));
+        Console.WriteLine(Base16ToDecimal("6D44"));
     }
 }
--- TP-03/Tests/UnitTest1.cs
+++ TP-03/Tests/UnitTest1.cs
@@ -2,5 +2,18 @@ namespace Tests;
 
 public class UnitTest1
 {
-
+    
+    
+    [Theory]
+    [InlineData("1 1 1", 1)]
+    [InlineData("0 1 2 2 4 5 5 7 7", 7)]
+    [InlineData("9 9 9 7 5 3 2 1 0", 9)]
+    [InlineData("1 3 4 6 8 9 7 5 3 0", 9)]
+    [InlineData("1 3 4 6 7 5 3 8", -1)]
+    [InlineData("1 2 3 2 1 2 5 5 1", -1)]
+    [InlineData("", -1)]
+    public void SomeTest(string n, int res)
+    {
+        Assert.Equal(res, KingOfTheHill(n));
+    }
 }
