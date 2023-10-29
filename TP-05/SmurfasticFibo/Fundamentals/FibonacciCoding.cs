3ae9348181d4debb13dfa90eafd57c547bb4ef5c
4m4Sec <neptune.dev.cybsec@gmail.com>
2023-10-29 21:39:37 +0100

    update [5.1.0] : fuck off

---
+++ TP-05/SmurfasticFibo/Fundamentals/FibonacciCoding.cs
@@ -0,0 +1,124 @@
+﻿namespace SmurfasticFibo.Fundamentals;
+
+public static class FibonacciCoding
+{
+
+    public static string EncodeCharacterFibo(char character)
+    {
+        string result = "1";
+        int k = 2;
+
+        if (character == '\0')
+            throw new ArgumentOutOfRangeException("Can't encode the ASCII number 0.");
+        
+        int x = (int)character;
+
+        if (x < 0)
+            throw new ArgumentOutOfRangeException("Character value must be upper than 0.");
+        if (x == 1)
+            return "10";
+
+        while (fibo(k) <= x)
+        {
+            k++;
+        }
+
+        for (int i = 1; i < k - 1; i++)
+        {
+            int fibKMinusI = fibo(k - i);
+            if (fibKMinusI <= x)
+            {
+                result = "1" + result;
+                x -= fibKMinusI;
+            }
+            else
+                result = "0" + result;
+            
+        }
+
+        return result;
+    }
+    
+    public static char DecodeCharacterFibo(string encodedCharacter)
+    {
+        
+        if (!encodedCharacter.EndsWith("11"))
+            throw new ArgumentException("The encoded character does not feature the end mark '11' at the end.");
+        
+        string encoded = encodedCharacter.Substring(0, encodedCharacter.Length - 2); // Remove the last '11'
+        int result = 0;
+        int i = encoded.Length - 1;
+        int fibKMinusI = 1;
+        
+        while (i >= 0)
+        {
+            if (encoded[i] == '1')
+                result += fibKMinusI;
+            
+            i--;
+            int temp = fibKMinusI;
+            fibKMinusI = fibKMinusI + result;
+            result = temp;
+        }
+
+        return (char)result;
+    }
+
+
+    public static int fibo(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        int a = 0;
+        int b = 1;
+        int temp;
+        for (int i = 2; i <= n; i++)
+        {
+            temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return b;
+    }
+
+    public static string EncodeFibo(string textToEncode)
+    {
+        string res = "";
+
+        foreach (char chr in textToEncode)
+        {
+            res += EncodeCharacterFibo(chr);
+        }
+
+        return res;
+    }
+
+    
+    public static string DecodeFibo(string encodedText)
+    {
+        if (encodedText.Length < 2)
+            throw new ArgumentException("Encoded text is too short.");
+        
+
+        string decodedText = "";
+        int startIndex = 0;
+
+        while (startIndex < encodedText.Length)
+        {
+            string encodedCharacter = "";
+
+            if (encodedCharacter.Length < 2 || !encodedCharacter.EndsWith("11"))
+                throw new ArgumentException("Invalid encoded character.");
+            
+
+            char decodedCharacter = DecodeCharacterFibo(encodedCharacter);
+            decodedText.Append(decodedCharacter);
+        }
+
+        return decodedText;
+    }
+}
--- TP-05/Tests/UnitTest1.cs
+++ TP-05/Tests/UnitTest1.cs
@@ -2,40 +2,83 @@ namespace Tests;
 
 public class UnitTest1
 {
- 
-    public static string Pyramid(ulong height, char block)
+
+   public static string EncodeCharacterFibo(char character)
+    {
+        string result = "1";
+        int k = 2;
+
+        if (character == '\0')
+            throw new ArgumentOutOfRangeException("Can't encode the ASCII number 0.");
+        
+        int x = (int)character;
+
+        if (x < 0)
+            throw new ArgumentOutOfRangeException("Character value must be upper than 0.");
+        if (x == 1)
+            return "10";
+
+        while (fibo(k) <= x)
+        {
+            k++;
+        }
+
+        for (int i = 1; i < k - 1; i++)
+        {
+            int fibKMinusI = fibo(k - i);
+            if (fibKMinusI <= x)
+            {
+                result = "1" + result;
+                x -= fibKMinusI;
+            }
+            else
+                result = "0" + result;
+            
+        }
+
+        return result;
+    }
+
+    public static int fibo(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        int a = 0;
+        int b = 1;
+        int temp;
+        for (int i = 2; i <= n; i++)
+        {
+            temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return b;
+    }
+
+    public static string EncodeFibo(string textToEncode)
     {
         string res = "";
 
-        for (ulong i = 0; i < height; i++)
+        foreach (char chr in textToEncode)
         {
-            for (ulong j = 0; j < height - i - 1; j++)
-            {
-                res += ' ';
-            }
-
-            for (ulong k = 0; k < 2 * i + 1; k++)
-            {
-                res += block;
-            }
-
-            for (ulong j = 0; j < height - i - 1; j++)
-            {
-                res += ' ';
-            }
-            
-            res += '\n';
+            res += EncodeCharacterFibo(chr);
         }
 
         return res;
     }
+
     
     [Theory]
-    [InlineData(1, '*', "*\n")]
-    [InlineData(2, '+', " + \n+++\n")]
-    [InlineData(3, '*', "  *  \n *** \n*****\n")]
-    public static void Test(ulong height, char block, string res)
+    [InlineData("123", "010001011001001011101001011")]
+    [InlineData("abc","000010000111000100001101001000011")]
+    [InlineData("Schtroumpfer c'est douter !", "010100101101001000011010001000111001001001110100010011100000100110101001001101010100011010000100110000010001110101000011101000100110010101101001000011000100011101010000110001001001110010010011001010110010100001110000010011010100100111001001001110101000011101000100110010101110101011")]
+    public static void Test(string str, string res)
     {
-        Assert.Equal(res, Pyramid(height, block));
+        
+        Assert.Equal(res, EncodeFibo(str));
     }
 }
