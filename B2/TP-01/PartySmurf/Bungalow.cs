d2c8e26427b12a338af616f40eadb4c984874db2
4m4Sec <neptune.dev.cybsec@gmail.com>
2023-11-22 12:49:12 +0100

    update [6.0.0] : add base structure for B2-TP-01

---
+++ B2/TP-01/PartySmurf/Bungalow.cs
@@ -0,0 +1,14 @@
+﻿namespace PartySmurf;
+
+public class Bungalow
+{
+    public static string[] CreateBungalow(string smurf1, string smurf2, string smurf3, string smurf4)
+    {
+        return new[] { smurf1, smurf2, smurf3, smurf4 };
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(CreateBungalow("brainy smurf", "hefty smurf", "clumsy smurf", "jokey smurf"));
+    }
+}
